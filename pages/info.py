1ced7852c2bec37b2ac9386772d2c752f67b3dd4
mutukinerjaupdlsbydashboard <mutukinerjaupdlsby.dashboard@gmail.com>
2026-04-27 15:06:13 +0700

    Update info.py

--- pages/info.py
+++ pages/info.py
@@ -14,23 +14,8 @@ def render_info():
     <div class='card'>
         <h4 style='margin-top: 0; color: #0078d4; margin-bottom: 15px;'>📶 Dashboard Learning Insight</h4>
         <p style='font-size: 0.9rem; line-height: 1.6;'>
-            Sistem ini dikembangkan untuk mempermudah proses tracking dan evaluasi ulasan peserta pembelajaran di <b>PT PLN (Persero) UPDL Surabaya</b>. 
-            Melalui integrasi dengan <b>Google Sheets</b>, sistem mampu melakukan otomasi pengolahan data yang terhubung langsung dengan dashboard <b>Streamlit</b>. 
-            Selain itu, sistem ini juga dilengkapi fitur <b>AI</b> untuk mengklasifikasikan sentimen, indikator, dan keluhan yang memungkinkan pengguna menambahkan file data dan 
+            Sistem ini dikembangkan untuk mempermudah proses evaluasi dengan mengklasifikasikan sentimen, indikator, dan keluhan sehingga tim mutu dapat menambahkan file data dan 
             menganalisis data teks secara otomatis untuk memperoleh hasilnya dengan lebih cepat dan akurat.
-        </p>
-    </div>
-    """, unsafe_allow_html=True)
-    
-    st.markdown("""
-    <div class='card'>
-        <h4 style='margin-top: 0; color: #0078d4; margin-bottom: 15px;'>🗃️ Data Management</h4>
-        <p style='font-size: 0.9rem; line-height: 1.6;'>
-            Data dikelola langsung oleh <b>Tim Evaluasi & Pengendalian Mutu dan Kinerja</b> secara terpusat melalui <b>Google Sheets</b> yang terintegrasi langsung dengan sistem Dashboard Learning Insight.
-            Data dapat diakses di:
-            <a href="https://docs.google.com/spreadsheets/d/1DtCoRzSXuXAM8MYBtiiSphNTzGplmgsPJEQfVWgRSe4/edit?usp=sharing" target="_blank" style="color:#0078d4; font-weight:500;">
-                Lihat Data
-            </a>
         </p>
     </div>
     """, unsafe_allow_html=True)
@@ -42,8 +27,7 @@ def render_info():
             <div style='background:rgba(0,120,212,0.05); padding:14px; border-radius:10px;'>
                 <b>🧠 Sistem NLP</b><br>
                 <small>
-                Sistem NLP dibuat menggunakan algoritma <b>RoBERTa (Robustly Optimized BERT Approach)</b>, yaitu model Deep Learning berbasis Transformers.
-                Model yang dibuat memiliki akurasi rata-rata <b>97-99%</b> baik untuk klasifikasi Sentimen, Indikator, dan Keluhan.
+                Sistem NLP dibuat menggunakan algoritma <b>RoBERTa dan IndoBERT. Model yang dibuat memiliki akurasi rata-rata <b>97-99%</b> baik untuk klasifikasi Sentimen, Indikator, dan Keluhan.
                 </small>
             </div>
             <div style='background:rgba(0,120,212,0.05); padding:14px; border-radius:10px;'>
@@ -60,7 +44,6 @@ def render_info():
     st.markdown("""
     <div class='card'>
         <h4 style='color: #0078d4; margin-top: 0; margin-bottom: 15px;'>👥 Tim</h4>
-        <p><b>Departemen Evaluasi & Pengendalian Mutu dan Kinerja</b><br>PT PLN (Persero) UPDL Surabaya</p>
-        <p><b>📧</b> evaluasi@pln.updl.ac.id</p>
+        <p><b>Divisi Evaluasi & Pengendalian Mutu dan Kinerja</b><br>PT PLN (Persero) UPDL Surabaya</p>
     </div>
     """, unsafe_allow_html=True)
